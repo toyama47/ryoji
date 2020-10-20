4eec9c75ad090762d912c67c18b8e2c274ea4e0a
toyama47 <65931339+toyama47@users.noreply.github.com>
2020-10-20 11:22:06 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,18 +1,3 @@
 // alert('Hello, toyama!');
 // alert(1+1);
-//alert(Math.sqrt(1+1));
-var who = "world";
-function sayhello() {
-    alert('Hello, ' + who + '!');
-}
-function taro() {
-    who = "太郎" ;
-    alert(who + 'が来た');
-}
-function hanako() {
-    who = "花子" ;
-    alert(who + 'が来た');
-}
-function saygoodbye() {
-    alert('Goodbye');
-}
+alert(Math.sqrt(1+1));
