8ac007a1e1b2bce76070747b33b7c8ef6088177d
toyama47 <65931339+toyama47@users.noreply.github.com>
2020-10-27 11:01:01 +0900

    Update ex06-5.js

--- ex06-5.js
+++ ex06-5.js
@@ -1,10 +1,11 @@
-function sayhello65() {
 var who65 = 'world';
-alert('Hello, ' + who65 + '!');
+
+function sayhello65() {
+ alert('Hello, ' + who65 + '!');
 }
 function taro65() {
-who65 = '太郎';
+ who65 = '太郎';
 }
 function hanako65() {
-who65 = '花子';
+  who65 = '花子';
 }
