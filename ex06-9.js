a1fa7a0a8533eabcd77d31dedc416bdf85665de9
toyama47 <65931339+toyama47@users.noreply.github.com>
2020-11-17 20:01:04 +0900

    Update ex06-9.js

--- ex06-9.js
+++ ex06-9.js
@@ -1,5 +1,2 @@
-function txtchange1() {
-document.getElementById('txt1').innerHTML = 'ナンバーワン！';}
-
-function imgchange1() {
-document.getElementById('logo').src = './image/bulbon.gif';}
+function txtchange1() 
+{document.getElementById('txt1').innerHTML= 'ナンバーワン!';}
