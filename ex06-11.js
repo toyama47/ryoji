6c022d175a146f6a489d38992b30704205e957cf
toyama47 <65931339+toyama47@users.noreply.github.com>
2021-01-26 12:13:50 +0900

    Update ex06-11.js

--- ex06-11.js
+++ ex06-11.js
@@ -1,7 +1,10 @@
-function hyoji1() { 
-var str = "一文字ずつ表示します。";
+
 var cnt = document.timer1.moji1.value.length;
+//文字が全部表示されているか確認
 if ( cnt < 11 ) { 
+//現在より1文字多く切り出して表示
 document.timer1.moji1.value = str.substr(0,cnt+1); } else {
+//全て表示されたら、空文字に戻す
 document.timer1.moji1.value = ""; }  
+//setTimeout()を含む関数を呼び出す
 setTimeout("hyoji1()",1000); }
