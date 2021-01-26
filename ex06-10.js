2954ffbc1a9ca16da6c08073c7a63f5a3f24ae79
toyama47 <65931339+toyama47@users.noreply.github.com>
2021-01-26 11:50:17 +0900

    Create ex06-10.js

---
+++ ex06-10.js
@@ -0,0 +1,6 @@
+function hyoji(){ var str = "一文字ずつ表示します。"; var cnt = document.timer.moji.value.length;if ( cnt < 11 ) 
+{document.timer.moji.value = str.substr(0,cnt+1);}else{ document.timer.moji.value = ""; } setTimeout("hyoji()",1000);
+}
+function mes(){
+alert("3秒経ちました！");
+}
