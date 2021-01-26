427654360e92da899e1b1efad341653250ab6371
toyama47 <65931339+toyama47@users.noreply.github.com>
2021-01-26 12:07:26 +0900

    Create ex06-12.js

---
+++ ex06-12.js
@@ -0,0 +1,4 @@
+function ChangeImageAndBack(){imgback612();ThreeSecChange612();}
+function ThreeSecChange612(){setTimeout("imgchange612()",3000);}
+function imgchange612() {document.getElementById('img13').src = './image/keiei_small.jpg';}
+function imgback612() {document.getElementById('img13').src = './image/josai.jpg';}
